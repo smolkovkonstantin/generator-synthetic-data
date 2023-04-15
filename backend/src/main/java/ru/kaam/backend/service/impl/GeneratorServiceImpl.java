dc489ca8b07e996f0511b66fa18258e37484e3ac
smolkovkonstantin <konstantin.w.smolkov@gmail.com>
2023-04-16 02:37:05 +0300

    added basic generator

--- backend/src/main/java/ru/kaam/backend/service/impl/GeneratorServiceImpl.java
+++ backend/src/main/java/ru/kaam/backend/service/impl/GeneratorServiceImpl.java
@@ -1,8 +1,40 @@
 package ru.kaam.backend.service.impl;
 
+import com.github.javafaker.Faker;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
+import ru.kaam.backend.dto.GeneratorDTO;
+import ru.kaam.backend.service.GeneratorService;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @Service
-public class GeneratorServiceImpl {
+@RequiredArgsConstructor
+public class GeneratorServiceImpl implements GeneratorService {
 
+    private final ApplicationContext context;
+
+    @Override
+    public Object generate(GeneratorDTO generatorDTO) {
+        Faker faker = new Faker();
+
+        Connection connection = (Connection) context.getBean("connection");
+        try {
+            Statement statement = connection.createStatement();
+
+            for (int i = 0; i < generatorDTO.count(); i++) {
+                Long latitude = faker.number().randomNumber();
+                Long longitude = faker.number().randomNumber();
+
+                statement.execute("INSERT INTO location_point(latitude, longitude)"
+                        + String.format(" VALUES (%s, %s)", latitude, longitude));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
 }
