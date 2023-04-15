62a2021f07c0cf872d518f9fe01653f00ff7c230
zxcum <90693604+zxcum@users.noreply.github.com>
2023-04-15 15:24:52 +0300

    basic interface

---
+++ frontend/src/Diagram.js
@@ -0,0 +1,8 @@
+import "./Diagram.css"
+function ER_Diagram(){
+    /* This function creates an ER diagram on the page*/
+    return(<div className="Diagram">
+        Here will be your diagram!!
+    </div>);
+}
+export default ER_Diagram;
