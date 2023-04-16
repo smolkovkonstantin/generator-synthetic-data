4c16f38f42db15cd18ae15a9dc3b0f20e7d28a57
smolkovkonstantin <konstantin.w.smolkov@gmail.com>
2023-04-16 13:24:39 +0300

    fix generate

--- backend/src/main/java/ru/kaam/backend/BackendApplication.java
+++ backend/src/main/java/ru/kaam/backend/BackendApplication.java
@@ -1,17 +1,8 @@
 package ru.kaam.backend;
 
-import com.fasterxml.jackson.annotation.JsonTypeName;
-import com.github.javafaker.Faker;
-import org.fluttercode.datafactory.impl.DataFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @SpringBootApplication
 public class BackendApplication {
